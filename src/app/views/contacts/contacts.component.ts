640977492168ec1f46e6db9e330ef9856df7b0c6
vitalii.martyniak <vitaliy7martynyak14@gmail.com>
2024-10-05 13:00:21 +0200

    Updated contacts page

--- src/app/views/contacts/contacts.component.ts
+++ src/app/views/contacts/contacts.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NavigationComponent
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
